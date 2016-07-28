1cd4b70bca04a605569011acee25aaba534f50fd
Denis Barushev <barushev@gmail.com>
2016-07-27 17:22:31 -0700

    Feature 5
    The user can select and move a shape from its current position to another position on the canvas.
    
    Build: http://barushev.net/shaper/builds/5/
    
    What's done:
    
    - Implemented dragging with mouse events.

--- src/components/Canvas.tsx
+++ src/components/Canvas.tsx
@@ -1,8 +1,6 @@
 import * as React from "react";
 
-import {Circle} from "./Circle.tsx";
-import {Square} from "./Square.tsx";
-import {Triangle} from "./Triangle.tsx";
+import {CanvasItem} from "./CanvasItem.tsx";
 
 import * as models from "../models.ts";
 
@@ -10,37 +8,41 @@ export interface CanvasProps {
 }
 
 export interface CanvasState {
-  shapes: models.Shape[];
+  shapes?: models.Shape[];
+  draggingShape?: models.Shape;
+  draggingShapeOffsetX?: number;
+  draggingShapeOffsetY?: number;
 }
 
 export class Canvas extends React.Component<CanvasProps, CanvasState> {
   constructor(props: CanvasProps) {
     super(props);
-    this.state = {shapes: []};
+    this.state = {
+      shapes: [],
+      draggingShapeOffsetX: 0,
+      draggingShapeOffsetY: 0
+    };
   }
 
   render() {
-    return <div className="canvas" onDrop={(e) => {this.drop(e)}} onDragOver={(e) => {this.dragOver(e)}}> 
+    return <div className="canvas"
+      onDragOver={(e) => {this.dragOver(e)}}
+      onDrop={(e) => {this.drop(e)}}
+      onMouseMove={(e) => {this.mouseMove(e)}}
+    >
       <svg ref="svg">
         {
-          this.state.shapes.map((shape) => {
-            return this.renderShape(shape);
+          this.state.shapes.map((shape, i) => {
+            return <CanvasItem
+              shape={shape}
+              key={i}
+              onMouseDown={(shape, e) => {this.shapeMouseDown(shape, e)}}
+              onMouseUp={(shape, e) => {this.shapeMouseUp(shape, e)}}
+            />
           })
         }
       </svg>
     </div>;
-  }
-
-  private renderShape(shape: models.Shape) {
-    let key = Math.random();
-
-    if (shape instanceof models.Circle) {
-      return <Circle shape={shape} key={key} />;
-    } else if (shape instanceof models.Square) {
-      return <Square shape={shape} key={key} />;
-    } else if (shape instanceof models.Triangle) {
-      return <Triangle shape={shape} key={key} />;
-    }
   }
 
   private dragOver(event: React.DragEvent) {
@@ -50,21 +52,53 @@ export class Canvas extends React.Component<CanvasProps, CanvasState> {
   private drop(event: React.DragEvent) {
     event.preventDefault();
 
-    let shapeType = event.dataTransfer.getData("shape");
     let shape: models.Shape;
-    let rect = (this.refs["svg"] as Element).getBoundingClientRect();
-    let centerX = event.pageX - rect.left;
-    let centerY = event.pageY - rect.top;
+    let shapeType = event.dataTransfer.getData("shape");
+
+    let {x, y} = this.getEventCoordinates(event);
     let size = 80;
 
     if (shapeType == "circle") {
-      shape = new models.Circle(centerX, centerY, size);
+      shape = new models.Circle(x, y, size);
     } else if (shapeType == "square") {
-      shape = new models.Square(centerX, centerY, size);
+      shape = new models.Square(x, y, size);
     } else if (shapeType == "triangle") {
-      shape = new models.Triangle(centerX, centerY, size);
+      shape = new models.Triangle(x, y, size);
     }
 
     this.setState({shapes: this.state.shapes.concat(shape)});
+  }
+
+  private mouseMove(event: React.MouseEvent) {
+    if (this.state.draggingShape) {
+      let {shapes} = this.state;
+      let shape = shapes[shapes.indexOf(this.state.draggingShape)];
+
+      let {x, y} = this.getEventCoordinates(event);
+
+      shape.x = x - this.state.draggingShapeOffsetX;
+      shape.y = y - this.state.draggingShapeOffsetY;
+
+      this.setState({shapes: shapes});
+    }
+  }
+
+  private shapeMouseDown(shape: models.Shape, event: React.MouseEvent) {
+    let {x, y} = this.getEventCoordinates(event);
+
+    this.setState({draggingShape: shape, draggingShapeOffsetX: x - shape.x, draggingShapeOffsetY: y - shape.y});
+  }
+
+  private shapeMouseUp(shape: models.Shape, event: React.MouseEvent) {
+    this.setState({draggingShape: null, draggingShapeOffsetX: 0, draggingShapeOffsetY: 0});
+  }
+
+  private getEventCoordinates(event: React.MouseEvent) {
+    const boundingRect = (this.refs["svg"] as Element).getBoundingClientRect();
+
+    return {
+      x: event.clientX - boundingRect.left,
+      y: event.clientY - boundingRect.top
+    };
   }
 }
