9b56f439a8f7c0a394832fde56fe1700da5f8261
Denis Barushev <barushev@gmail.com>
2016-08-03 10:33:15 -0700

    Make window listen to mousemove and mouseup when moving shape.
    As per @danschultz advice in code review.

--- src/components/Canvas.tsx
+++ src/components/Canvas.tsx
@@ -9,7 +9,7 @@ export interface CanvasProps {
 
 export interface CanvasState {
   shapes?: models.Shape[];
-  draggingShape?: models.Shape;
+  draggingShapeIndex?: number;
   draggingShapeOffsetX?: number;
   draggingShapeOffsetY?: number;
 }
@@ -17,18 +17,21 @@ export interface CanvasState {
 export class Canvas extends React.Component<CanvasProps, CanvasState> {
   constructor(props: CanvasProps) {
     super(props);
+
     this.state = {
       shapes: [],
       draggingShapeOffsetX: 0,
       draggingShapeOffsetY: 0
     };
+
+    this.windowMouseMove = this.windowMouseMove.bind(this);
+    this.windowMouseUp = this.windowMouseUp.bind(this);
   }
 
   render() {
     return <div className="canvas"
       onDragOver={(e) => {this.dragOver(e)}}
       onDrop={(e) => {this.drop(e)}}
-      onMouseMove={(e) => {this.mouseMove(e)}}
     >
       <svg ref="svg">
         {
@@ -37,7 +40,6 @@ export class Canvas extends React.Component<CanvasProps, CanvasState> {
               shape={shape}
               key={i}
               onMouseDown={(shape, e) => {this.shapeMouseDown(shape, e)}}
-              onMouseUp={(shape, e) => {this.shapeMouseUp(shape, e)}}
             />
           })
         }
@@ -69,31 +71,43 @@ export class Canvas extends React.Component<CanvasProps, CanvasState> {
     this.setState({shapes: this.state.shapes.concat(shape)});
   }
 
-  private mouseMove(event: React.MouseEvent) {
-    if (this.state.draggingShape) {
-      let {shapes} = this.state;
-      let shape = shapes[shapes.indexOf(this.state.draggingShape)];
+  private shapeMouseDown(shape: models.Shape, event: React.MouseEvent) {
+    let {x, y} = this.getEventCoordinates(event);
 
+    this.setState({
+      draggingShapeIndex: this.state.shapes.indexOf(shape),
+      draggingShapeOffsetX: x - shape.x,
+      draggingShapeOffsetY: y - shape.y
+    });
+
+    window.addEventListener("mousemove", this.windowMouseMove);
+    window.addEventListener("mouseup", this.windowMouseUp);
+  }
+
+  private windowMouseMove(event: MouseEvent) {
+    if (this.state.draggingShapeIndex != null) {
+      let shape = this.state.shapes[this.state.draggingShapeIndex];
       let {x, y} = this.getEventCoordinates(event);
 
       shape.x = x - this.state.draggingShapeOffsetX;
       shape.y = y - this.state.draggingShapeOffsetY;
 
-      this.setState({shapes: shapes});
+      this.setState({shapes: this.state.shapes});
     }
   }
 
-  private shapeMouseDown(shape: models.Shape, event: React.MouseEvent) {
-    let {x, y} = this.getEventCoordinates(event);
+  private windowMouseUp(event: MouseEvent) {
+    this.setState({
+      draggingShapeIndex: null,
+      draggingShapeOffsetX: 0,
+      draggingShapeOffsetY: 0
+    });
 
-    this.setState({draggingShape: shape, draggingShapeOffsetX: x - shape.x, draggingShapeOffsetY: y - shape.y});
+    window.removeEventListener("mousemove", this.windowMouseMove);
+    window.removeEventListener("mouseup", this.windowMouseUp);
   }
 
-  private shapeMouseUp(shape: models.Shape, event: React.MouseEvent) {
-    this.setState({draggingShape: null, draggingShapeOffsetX: 0, draggingShapeOffsetY: 0});
-  }
-
-  private getEventCoordinates(event: React.MouseEvent) {
+  private getEventCoordinates(event: MouseEvent | React.MouseEvent) {
     const boundingRect = (this.refs["svg"] as Element).getBoundingClientRect();
 
     return {
